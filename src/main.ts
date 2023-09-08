b0a448708f81d40e2be2dda9590dcabe2eafb3ee
Zerio <neoboij@gmail.com>
2023-09-08 20:32:37 +0200

    setup database saving/fetching

--- src/main.ts
+++ src/main.ts
@@ -1,56 +1,88 @@
 // imports
 import { Client, GatewayIntentBits, Message } from "discord.js";
 import { config as dotenvSetup } from "dotenv";
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
 
 // data
 const replies: Record<string, string> = {};
 
-// setup
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.GuildPresences,
-    GatewayIntentBits.GuildMessageReactions,
-    GatewayIntentBits.DirectMessages,
-    GatewayIntentBits.MessageContent,
-  ],
-});
-const result = dotenvSetup();
+(async () => {
+  // setup
+  const client = new Client({
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.GuildPresences,
+      GatewayIntentBits.GuildMessageReactions,
+      GatewayIntentBits.DirectMessages,
+      GatewayIntentBits.MessageContent,
+    ],
+  });
+  const dotenvResult = dotenvSetup();
 
-// main
-if (result.error) {
-  console.log("dotenv error", result.error);
-} else {
-  if (process.env.TOKEN) {
-    client.on("ready", () => {
-      if (client.user) {
-        console.log(`[SUCCESS] Logged in as ${client.user.tag}!`);
-      }
-    });
+  const db = await open({
+    filename: "data.db",
+    driver: sqlite3.Database,
+  });
 
-    client.on("messageCreate", async (message: Message<boolean>) => {
-      let found: undefined | string = undefined;
+  // db handling
+  sqlite3.verbose();
 
-      for (const key in replies) {
-        if (message.content.includes(key)) {
-          found = replies[key];
-        }
-      }
+  await db.run(`
+    create table if not exists replies (
+      id integer not null primary key autoincrement,
+      message text not null,
+      reply text not null,
+      unique(id, message)
+    )
+  `);
 
-      if (found) {
-        message.reply(found);
-      }
-    });
+  const repliesResult = await db.all(`
+    select * from replies
+  `);
 
-    client.login(process.env.TOKEN).catch((err: string) => {
-      console.error(
-        "[CRASH] Something went wrong while connecting to your bot...",
-      );
-      console.error("[CRASH] Error from Discord API:" + err);
-      return process.exit();
-    });
-  } else {
-    console.log("invalid token");
+  for (let i = 0; i < repliesResult.length; i++) {
+    const data = repliesResult[i];
+    replies[data.message] = data.reply;
   }
-}
+
+  // main
+  if (dotenvResult.error) {
+    console.log("dotenv error", dotenvResult.error);
+  } else {
+    if (process.env.TOKEN) {
+      client.on("ready", () => {
+        if (client.user) {
+          console.log(`[SUCCESS] Logged in as ${client.user.tag}!`);
+        }
+      });
+
+      client.on("messageCreate", async (message: Message<boolean>) => {
+        if (message.author.bot == false) {
+          let found: undefined | string = undefined;
+
+          for (const key in replies) {
+            if (message.content.includes(key)) {
+              found = replies[key];
+            }
+          }
+
+          if (found) {
+            message.reply(found);
+          }
+        }
+      });
+
+      client.login(process.env.TOKEN).catch((err: string) => {
+        console.error(
+          "[CRASH] Something went wrong while connecting to your bot...",
+        );
+        console.error("[CRASH] Error from Discord API:" + err);
+        return process.exit();
+      });
+    } else {
+      console.log("invalid token");
+    }
+  }
+})();
